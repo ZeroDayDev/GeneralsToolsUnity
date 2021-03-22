025e6aebceebbef3d8804ca401dd43b5a88e82b1
Kleyver De León (cseliteGTyZX) <81109179+cseliteGTyZX@users.noreply.github.com>
2021-03-22 11:53:20 -0600

    Añadida la herramienta "CameraShake"
    Herramienta creada para agitar la cámara a través de una única función.

---
+++ CameraShake.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    //Script creado por cseliteGTyZX(Kleyver De León), usalo para lo que quieras.
+    //Script created by cseliteGTyZX(Kleyver De León), use this for you want.
+
+    public static void ShakeCamera(float shakeForce, GameObject Camera)
+    {
+        //Here the temporal vector, influenced by a random value multiplied by the shakeforce.
+        Vector2 tempVector = Random.insideUnitSphere * shakeForce;
+
+        //Here the camera move in local, for simulate the shake.
+        Camera.transform.localPosition = new Vector3(tempVector.x, tempVector.y, Camera.transform.position.z);
+    }
+}
+
